4ff912b254f0d0e41f641dada14905ccf22e8305
Juha Möttönen <juha.mottonen@mainio.co>
2018-08-06 14:26:10 +0300

    Services are now accessible

--- projects/mainio-chat-bot/src/public_api.ts
+++ projects/mainio-chat-bot/src/public_api.ts
@@ -3,3 +3,7 @@
  */
 
 export * from "./lib/mainio-chat-bot.module";
+export * from "./lib/services/services.module";
+export * from "./lib/services/chat-bot.service";
+export * from "./lib/services/chat-bot-action.service";
+export * from "./lib/services/bot-actions-completed.service";
