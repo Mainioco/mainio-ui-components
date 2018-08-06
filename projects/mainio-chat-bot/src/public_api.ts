301c5e173ebc462bf2549dfed3f13ce91ea68a86
Juha Möttönen <juha.mottonen@mainio.co>
2018-08-07 02:24:04 +0300

    Export types instead anys

--- projects/mainio-chat-bot/src/public_api.ts
+++ projects/mainio-chat-bot/src/public_api.ts
@@ -1,13 +1,32 @@
 /*
  * Public API Surface of mainio-chat-bot
  */
-export * from "./lib/mainio-chat-bot.module";
-export * from "./lib/components/chat-bot/mainio-chat-bot.component";
-export * from "./lib/components/chat-bot/chat-bot-components/action-row/action-row.component";
-export * from "./lib/services/bot-actions-completed.service";
-export * from "./lib/services/chat-bot-action.service";
-export * from "./lib/services/chat-bot.service";
-export * from "./lib/services/models/ask-input";
-export * from "./lib/services/bot-response-parameter-parser/iparser";
-export * from "./lib/services/bot-response-parameter-parser/parameter-parser.service";
-export * from "./lib/services/bot-response-parameter-parser/url-parser.service";
+export { MainioChatBotModule } from "./lib/mainio-chat-bot.module";
+export {
+  MainioChatBotComponent
+} from "./lib/components/chat-bot/mainio-chat-bot.component";
+export {
+  ActionRowComponent
+} from "./lib/components/chat-bot/chat-bot-components/action-row/action-row.component";
+export {
+  BotActionsCompletedService
+} from "./lib/services/bot-actions-completed.service";
+export {
+  ChatBotActionService,
+  CachedValues
+} from "./lib/services/chat-bot-action.service";
+export {
+  ChatBotService,
+  IAskOutputDto,
+  BotData,
+  BotStatus,
+  AskOutputDto
+} from "./lib/services/chat-bot.service";
+export { AskInput } from "./lib/services/models/ask-input";
+export { IParser } from "./lib/services/bot-response-parameter-parser/iparser";
+export {
+  ParameterParserService
+} from "./lib/services/bot-response-parameter-parser/parameter-parser.service";
+export {
+  UrlParserService
+} from "./lib/services/bot-response-parameter-parser/url-parser.service";
